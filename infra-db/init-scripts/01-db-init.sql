13ff0a4348eb65cfef656a20fa24e253e68346d3
thilinadj <thilinadjayasuriya@gmail.com>
2026-02-28 13:45:42 +0530

    feat: Add initial SQL script to create identity, salary, and community schemas for microservice isolation.

--- infra-db/init-scripts/01-db-init.sql
+++ infra-db/init-scripts/01-db-init.sql
@@ -1,5 +1,4 @@
 -- Create schemas for microservice isolation
-CREATE SCHEMA IF NOT EXISTS vote_schema;
 CREATE SCHEMA IF NOT EXISTS identity_schema;
 CREATE SCHEMA IF NOT EXISTS salary_schema;
 CREATE SCHEMA IF NOT EXISTS community_schema;
